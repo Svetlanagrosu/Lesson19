ac2eebb8628fd2bf4225c5f0921fb1bf14d9ea75
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-21 21:59:07 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+Ex2
+const numberArr = [4,9,8,3,7,5]
+
+const obj = {
+    nume: 'Leon',
+    age: 50,
+    place: 'Sahara'
+}
+numberArr.forEach((item) => console.log(item))
+
+//for(let item in obj) {
+    //console.log(obj[item])
+
+//}
+
+
+
+
+
+
+
+
+//Ex3
+//const obj = {
+   // nume: 'Leon',
+   // age: 50,
+   // color: 'brown',
+    //place: 'Amazonia',
+//}
+
+//const obj1 = { ...obj,nume: 'Bear', place: 'Taiga',weight: 100}
+
+//console.log(obj,obj1)
+
+
+
+//Ex4
+//const numberArr = [4,3,6,8,9,7]
+
+//const arrSum = numberArr.reduce((acc, curr, index, arr) => {
+    //console.log('acc:',acc, 'cure:' ,curr, 'index:' ,index)
+
+    //return curr + 1
+
+//}, 0)
+//console.log('result is:', arrSum)
