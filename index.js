8749bb195821e9a8d57ff5494401365464354deb
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-24 19:59:12 +0100

    n commit

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-//Ex2
-//const numberArr = [4,9,8,3,7,5]
-//const obj = {
-    //nume: 'Leon',
-    //age: 50,
-    //place: 'Sahara'
-//}
-//for(let item in obj) {
-    //console.log(obj[item])
-//}
-
-//Ex3
-//const obj = {
-    //nume: 'Leon',
-    //age: 50,
-    //color: 'brown',
-    //place: 'Amazonia',
-//}
-//const obj1 = { ...obj,nume: 'Bear', place: 'Taiga',weight: 100}
-
-//console.log(obj,obj1)
 
 
-//Ex4
-//const numberArr = [4,3,6,8,9,7]
 
-//const arrSum = numberArr.reduce((acc, curr, index, arr) => {
-   // console.log('acc:',acc, 'cure:' ,curr, 'index:' ,index)
+//Ex4 Creati o functie care primeste un array de cuffre si va intoarce acelas array
+      //doar ca fiecarui element ii va fi adauga 1.
 
-   // return curr + 1
-
-//}, 0)
-//console.log('result is:', arrSum)
-
+Ex 4.2 (corect)
 const arr = [1,2,3,4,5,6]
 
 const func = (list) => {
