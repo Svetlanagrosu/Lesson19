a082e01b212b7bba3993db8be2819bdff23c31b8
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-24 19:53:53 +0100

    n commit

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-Ex2
-const numberArr = [4,9,8,3,7,5]
-const obj = {
-    nume: 'Leon',
-    age: 50,
-    place: 'Sahara'
+//Ex2
+//const numberArr = [4,9,8,3,7,5]
+//const obj = {
+    //nume: 'Leon',
+    //age: 50,
+    //place: 'Sahara'
+//}
+//for(let item in obj) {
+    //console.log(obj[item])
+//}
+
+//Ex3
+//const obj = {
+    //nume: 'Leon',
+    //age: 50,
+    //color: 'brown',
+    //place: 'Amazonia',
+//}
+//const obj1 = { ...obj,nume: 'Bear', place: 'Taiga',weight: 100}
+
+//console.log(obj,obj1)
+
+
+//Ex4
+//const numberArr = [4,3,6,8,9,7]
+
+//const arrSum = numberArr.reduce((acc, curr, index, arr) => {
+   // console.log('acc:',acc, 'cure:' ,curr, 'index:' ,index)
+
+   // return curr + 1
+
+//}, 0)
+//console.log('result is:', arrSum)
+
+const arr = [1,2,3,4,5,6]
+
+const func = (list) => {
+    return list.reduce((acc,cur )=> [...acc,cur + 1], [])
 }
-for(let item in obj) {
-    console.log(obj[item])
-}
+console.log(func(arr))
+ 
+    
 
-Ex3
-const obj = {
-    nume: 'Leon',
-    age: 50,
-    color: 'brown',
-    place: 'Amazonia',
-}
-const obj1 = { ...obj,nume: 'Bear', place: 'Taiga',weight: 100}
-
-console.log(obj,obj1)
+    
 
 
-Ex4
-const numberArr = [4,3,6,8,9,7]
 
-const arrSum = numberArr.reduce((acc, curr, index, arr) => {
-    console.log('acc:',acc, 'cure:' ,curr, 'index:' ,index)
 
-    return curr + 1
+ 
+    
+        
 
-}, 0)
-console.log('result is:', arrSum)
